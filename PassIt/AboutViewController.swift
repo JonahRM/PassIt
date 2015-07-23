4c3a84a445dba75bd0276f07d05759cffe132e64
Jonah May <jrmay@princeton.edu>
2015-07-23 13:29:12 -0700

    added working nav feature

---
+++ PassIt/AboutViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.swift
+//  PassIt
+//
+//  Created by Jonah May on 7/23/15.
+//  Copyright © 2015 Jonah May. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+    
+    
+    @IBAction func goBack(sender: UIBarButtonItem) {
+            print("goint back from about to home")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+    
+
+}
--- PassIt/HomeViewController.swift
+++ PassIt/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PassIt
 //
 //  Created by Jonah May on 7/22/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
